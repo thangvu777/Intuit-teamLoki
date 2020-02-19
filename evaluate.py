a52fb07fffc3d722ec29758949965a819ef86e23
briannoogin <brian.qh.nguyen@gmail.com>
2020-02-19 13:22:18 -0600

    pipeline should be finished, able to make csv file

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,8 @@ from pandas import ExcelFile
 from PIL import Image
 import os
 import time
+import csv
+
 if __name__ == '__main__':
     folder_list = ["W2_Clean_DataSet_01_20Sep2019", "W2_Noise_DataSet_01_20Sep2019"]
     truth_list = ['W2_Truth_and_Noise_DataSet_01.xlsx', "W2_Truth_and_Noise_DataSet_02.xlsx"]
@@ -20,25 +22,32 @@ if __name__ == '__main__':
 
         files = sorted(os.listdir(folder_path))
         index = 0
-        for file in files:
-            # filter based on images
-            if file.endswith('.png') or file.endswith('.jpg'):
-                file = os.path.join(folder_path, file)
-                doc = docs[index]
-                doc_name = doc['File_BaseName']
-                start_time = time.time()
-                parse = pytesseract.image_to_string(Image.open(file))
-                end_time = time.time()
-                num_correct = 0
-                num_total = 0
-                field_names = doc.values()
-                for field_name in field_names:
-                    if str(field_name) in parse:
-                        num_correct += 1
-                    num_total += 1
-                print(doc_name)
-                print("Accuracy", (num_correct / num_total) * 100)
-                print("Time to parse document: {} seconds".format(end_time - start_time))
-                index += 1
+        with open("results.csv", 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            # write the headers
+            writer.writerow(['Document Name', 'Accuracy', 'Time (seconds)'])
+            for file in files:
+                # filter based on images
+                if file.endswith('.png') or file.endswith('.jpg'):
+                    file = os.path.join(folder_path, file)
+                    doc = docs[index]
+                    doc_name = doc['File_BaseName']
+                    start_time = time.time()
+                    parse = pytesseract.image_to_string(Image.open(file))
+                    end_time = time.time()
+                    num_correct = 0
+                    num_total = 0
+                    field_names = doc.values()
+                    for field_name in field_names:
+                        if str(field_name) in parse:
+                            num_correct += 1
+                        num_total += 1
+                    accuracy = (num_correct / num_total) * 100
+                    time_spent = end_time - start_time
+                    print(doc_name)
+                    print("Accuracy", accuracy)
+                    print("Time to parse document: {} seconds".format(time_spent))
+                    writer.writerow([doc_name, accuracy, time_spent])
+                    index += 1
 
 
