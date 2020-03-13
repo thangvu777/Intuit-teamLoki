645b2acce19a42635efced0e1645d4f6ccaec971
Thang Vu <thang@Thangs-iMac.local>
2020-03-13 00:11:30 -0500

    skew fix works for singular blocks of texts

--- fixSkew.py
+++ fixSkew.py
@@ -4,7 +4,7 @@ import time
 import cv2
 import os
 import numpy as np
-
+from scipy.ndimage import interpolation as inter
 
 def create_bounding_boxes(image_name:str, full_image_dir:str, output_dir:str) -> None:
     img = cv2.imread(full_image_dir)
@@ -25,6 +25,32 @@ def create_bounding_boxes(image_name:str, full_image_dir:str, output_dir:str) ->
     #cv2.imsave(os.path.join(output_dir,image_name), img)
     cv2.imshow(full_image_dir, img)
     cv2.waitKey(0)
+
+def correct_skew(image, delta=1, limit=5):
+    def determine_score(arr, angle):
+        data = inter.rotate(arr, angle, reshape=False, order=0)
+        histogram = np.sum(data, axis=1)
+        score = np.sum((histogram[1:] - histogram[:-1]) ** 2)
+        return histogram, score
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    scores = []
+    angles = np.arange(-limit, limit + delta, delta)
+    for angle in angles:
+        histogram, score = determine_score(thresh, angle)
+        scores.append(score)
+
+    best_angle = angles[scores.index(max(scores))]
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, best_angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, \
+              borderMode=cv2.BORDER_REPLICATE)
+
+    return best_angle, rotated
 
 def fix_skew(img: str):
     skew_output = 'data/skew/fix_skew_output/'
@@ -54,6 +80,54 @@ def fix_skew(img: str):
     cv2.imshow("Rotated", rotated)
     cv2.waitKey(0)
 
+def fix_skew2(img:str) -> None:
+    image = cv2.imread(img)
+    # convert the image to grayscale and flip the foreground
+    # and background to ensure foreground is now "white" and
+    # the background is "black"
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
+    gray = cv2.bitwise_not(gray)
+    # threshold the image, setting all foreground pixels to
+    # 255 and all background pixels to 0
+    #thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnt = contours[0]
+    x,y,w,h = cv2.boundingRect(cnt)
+    crop = image[y:y + h, x:x + w]
+    image = crop
+    # grab the (x, y) coordinates of all pixel values that
+    # are greater than zero, then use these coordinates to
+    # compute a rotated bounding box that contains all
+    # coordinates
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    # the `cv2.minAreaRect` function returns values in the
+    # range [-90, 0); as the rectangle rotates clockwise the
+    # returned angle trends to 0 -- in this special case we
+    # need to add 90 degrees to the angle
+    if angle < -45:
+        angle = -(90 + angle)
+    # otherwise, just take the inverse of the angle to make
+    # it positive
+    else:
+        angle = -angle
+    # rotate the image to deskew it
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h),
+                             flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    # draw the correction angle on the image so we can validate it
+    cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
+                (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    # show the output image
+    print("[INFO] angle: {:.3f}".format(angle))
+    cv2.imshow("Input", image)
+    cv2.imshow("Rotated", rotated)
+    cv2.waitKey(0)
+
 def process ( directory : str , fix_skew : bool) -> None:
     for filename in sorted(os.listdir(directory)):
         start_time = time.time()
@@ -72,6 +146,9 @@ def process ( directory : str , fix_skew : bool) -> None:
         print('========================================================')
 
 if __name__ == '__main__':
-    dir = 'data/skewed/W2_XL_input_noisy_1017.jpg'
+    #dir = 'data/skewed/W2_Multi_Sample_Data_input_ADP1_noisy_15677.jpg'
+    #dir = 'data/skewed/5.png'
     #create_bounding_boxes('hey', dir, 'out')
+
+    dir = 'data/skewed/W2_XL_input_noisy_10291.jpg'
     fix_skew(dir)
