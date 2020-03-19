b02a861bcfc8970350dd06044c93430ca70d9ab8
Thang Vu <thang@Thangs-iMac.local>
2020-03-19 12:11:28 -0500

    adjusted rotation for cw, ccw, and straight images

--- fixSkew.py
+++ fixSkew.py
@@ -7,42 +7,13 @@ import numpy as np
 import math
 import PIL
 
-def fix_skew(img: str):
-    skew_output = 'data/skew/fix_skew_output/'
-    img = cv2.imread(img)
-    # convert the image to grayscale and flip the foreground
-    # background -> black; foreground -> white
-    # background pixels = 0; foreground pixels = 255
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_not(gray)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    coords = np.column_stack(np.where(thresh > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-	    angle = -angle
-    # rotate the image to deskew it
-    (h, w) = img.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
-                (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    # show the output image
-    print("[INFO] angle: {:.3f}".format(angle))
-    cv2.imshow("Input", img)
-    cv2.imshow("Rotated", rotated)
-    cv2.waitKey(0)
-
 def process (directory : str, outD:str) -> None:
     for filename in sorted(os.listdir(directory)):
         start_time = time.time()
         if filename.endswith('.jpg'):
             file = os.path.join(directory, filename)
             print('Fixing Skew of %s: ' %(file))
-            img = fix_skew(file)
-            cv2.imwrite(os.path.join(outD, filename))
+            fix_skew(file)
         else:
             continue
         end_time = time.time()
@@ -61,9 +32,13 @@ def fix_skew(img: str):
     2. Get the dimensions of the image using shape
     3. Find the approximate Cartesian coordinates of the image  
     4. Calculate the angle between 3 points (2 points from the image, 1 from (0,0))
-    5. Rotate and crop the image using the center of the original image
+    5. Check for straight image, if image is crooked, rotation will occur
+    6. Rotate and crop the image using the center of the original image
 '''
 def fix_skew_helper(img: str):
+    # save path string
+    path = img
+
     # load image
     img = cv2.imread(img)
 
@@ -90,11 +65,13 @@ def fix_skew_helper(img: str):
             area = area_thresh
             big_contour = c
 
-    ''' # Uncomment if you want to see the contour lines
+    '''
+    # Uncomment if you want to see the contour lines
     # (RED) draw the contour on a copy of the input image
     results = img.copy()
     cv2.drawContours(results, [big_contour], 0, (0, 0, 255), 2)
-    cv2.imshow('results', results)
+    out = str('Contouring     ' + path)
+    cv2.imshow(out, results)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     '''
@@ -103,8 +80,9 @@ def fix_skew_helper(img: str):
     dimensions = img.shape
     height = img.shape[0]
     width = img.shape[1]
-
+    print ("w: %d h: %d" %(width, height))
     # STEP 3: Find approximate coordinates of outlining box
+        # .05 approx level gives us the 4 coordinates of the W-2 Rectangular shape
     approx = cv2.approxPolyDP(big_contour, .05 * cv2.arcLength(big_contour, True), True)
     # Used to flatten the array containing the co-ordinates of the vertices.
     n = approx.ravel()
@@ -116,14 +94,34 @@ def fix_skew_helper(img: str):
             y = n[i + 1]
         i = i + 1
         points.append([x,y])
-        print(x,y)
+        #print(x,y)
 
     # STEP 4: Calculate degree of rotation
     # FUNCTION DEFINITION: Calculate an angle given 3 Cartesian coordinates
     def getAngle(a, b, c):
         ang = math.degrees(math.atan2(c[1] - b[1], c[0] - b[0]) - math.atan2(a[1] - b[1], a[0] - b[0]))
         return ang + 360 if ang < 0 else ang
-    angle = (getAngle([0,0], points[0], points[2])) - 360
+
+    '''
+    This is for images that don't necessarily need to be rotated since they already stand correctly
+         .00166 is equal to 1/600
+         ex. Original Dimensions (w x h): 2005 x 2340
+         1/600 * (2005) = 3.33; 1/600 * (2340) = 3.88
+         If one of the vertices of the image lies at approximately 2004 x 2339
+         the image is most likely straight because the absolute difference is 1 pixel apart 
+    '''
+    if (abs(width - points[4][0]) <= .00166 * width and abs(height - points[4][1] <= .00166 * height)):
+        angle = 0
+    else: # Rotation needs to occur
+        # This is for images rotated counter-clockwise originally with respect to the center
+        # X1 > Y2: ex. P1 = (1221,0); P2 = (0,172) -> True
+        if (points[0][0] >= points[2][1]):
+            angle = (getAngle([0, 0], points[0], points[2]))
+        # This is for images rotated clockwise originally with respect to the center
+        # X1 < Y1: ex. P1 = (405,0); P2 = (0,2303) -> True
+        elif (points[0][0] < points[2][1]):
+            angle = (getAngle([0, 0], points[0], points[2])) + 90
+    #print('angle = %.3f' % angle)
 
     # STEP 5: Rotate and Crop
     # FUNCTION DEFINITION: Rotate a given image around the center with angle theta in degrees,
@@ -145,12 +143,13 @@ def fix_skew_helper(img: str):
 
     rotated_image = rotate(img, center=(width/2, height/2), theta=angle, width= width, height=height)
 
-    ''' # Uncomment if you want to see the rotated image
-    cv2.imshow('rotated',rotated_image)
+    '''
+     # Uncomment if you want to see the rotated image
+    out = str('Rotated     ' + path)
+    cv2.imshow(out, rotated_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     '''
-
     # Return the final rotated image
     return rotated_image
 
@@ -159,4 +158,5 @@ if __name__ == '__main__':
     outD = 'data/skewed/rotatefix'
     if not os.path.exists(outD):
         os.mkdir(outD)
-    process(dir, outD)
+    #img = fix_skew('data/skewed/W2_Multi_Sample_Data_input_ADP1_noisy_15677.jpg')
+    process('data/fake-w2-us-tax-form-dataset/w2_samples_multi_noisy','out')
