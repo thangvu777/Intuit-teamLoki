0fa2f57d27827411adbd44377c3c73a8bca1fdac
Thang Vu <thang@Thangs-iMac.local>
2020-03-19 12:17:23 -0500

    Added rotations for CW, CCW, and straight images

--- evaluate.py
+++ evaluate.py
@@ -126,9 +126,14 @@ def fix_skew(img: str):
     2. Get the dimensions of the image using shape
     3. Find the approximate Cartesian coordinates of the image  
     4. Calculate the angle between 3 points (2 points from the image, 1 from (0,0))
-    5. Rotate and crop the image using the center of the original image
+    5. Check for straight image, if image is crooked, rotation will occur
+    6. Rotate and crop the image using the center of the original image
 '''
+
 def fix_skew_helper(img: str):
+    # save path string
+    path = img
+
     # load image
     img = cv2.imread(img)
 
@@ -155,11 +160,13 @@ def fix_skew_helper(img: str):
             area = area_thresh
             big_contour = c
 
-    ''' # Uncomment if you want to see the contour lines
+    '''
+    # Uncomment if you want to see the contour lines
     # (RED) draw the contour on a copy of the input image
     results = img.copy()
     cv2.drawContours(results, [big_contour], 0, (0, 0, 255), 2)
-    cv2.imshow('results', results)
+    out = str('Contouring     ' + path)
+    cv2.imshow(out, results)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     '''
@@ -168,8 +175,9 @@ def fix_skew_helper(img: str):
     dimensions = img.shape
     height = img.shape[0]
     width = img.shape[1]
-
+    print ("w: %d h: %d" %(width, height))
     # STEP 3: Find approximate coordinates of outlining box
+        # .05 approx level gives us the 4 coordinates of the W-2 Rectangular shape
     approx = cv2.approxPolyDP(big_contour, .05 * cv2.arcLength(big_contour, True), True)
     # Used to flatten the array containing the co-ordinates of the vertices.
     n = approx.ravel()
@@ -181,13 +189,34 @@ def fix_skew_helper(img: str):
             y = n[i + 1]
         i = i + 1
         points.append([x,y])
+        #print(x,y)
 
     # STEP 4: Calculate degree of rotation
     # FUNCTION DEFINITION: Calculate an angle given 3 Cartesian coordinates
     def getAngle(a, b, c):
         ang = math.degrees(math.atan2(c[1] - b[1], c[0] - b[0]) - math.atan2(a[1] - b[1], a[0] - b[0]))
         return ang + 360 if ang < 0 else ang
-    angle = (getAngle([0,0], points[0], points[2])) - 360
+
+    '''
+    This is for images that don't necessarily need to be rotated since they already stand correctly
+         .00166 is equal to 1/600
+         ex. Original Dimensions (w x h): 2005 x 2340
+         1/600 * (2005) = 3.33; 1/600 * (2340) = 3.88
+         If one of the vertices of the image lies at approximately 2004 x 2339
+         the image is most likely straight because the absolute difference is 1 pixel apart 
+    '''
+    if (abs(width - points[4][0]) <= .00166 * width and abs(height - points[4][1] <= .00166 * height)):
+        angle = 0
+    else: # Rotation needs to occur
+        # This is for images rotated counter-clockwise originally with respect to the center
+        # X1 > Y2: ex. P1 = (1221,0); P2 = (0,172) -> True
+        if (points[0][0] >= points[2][1]):
+            angle = (getAngle([0, 0], points[0], points[2]))
+        # This is for images rotated clockwise originally with respect to the center
+        # X1 < Y1: ex. P1 = (405,0); P2 = (0,2303) -> True
+        elif (points[0][0] < points[2][1]):
+            angle = (getAngle([0, 0], points[0], points[2])) + 90
+    #print('angle = %.3f' % angle)
 
     # STEP 5: Rotate and Crop
     # FUNCTION DEFINITION: Rotate a given image around the center with angle theta in degrees,
@@ -209,8 +238,10 @@ def fix_skew_helper(img: str):
 
     rotated_image = rotate(img, center=(width/2, height/2), theta=angle, width= width, height=height)
 
-    ''' # Uncomment if you want to see the rotated image
-    cv2.imshow('rotated',rotated_image)
+    '''
+     # Uncomment if you want to see the rotated image
+    out = str('Rotated     ' + path)
+    cv2.imshow(out, rotated_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     '''
